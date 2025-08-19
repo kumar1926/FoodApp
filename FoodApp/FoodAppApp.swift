35c2443aa8e6d818e6684547076b2a1a5453c7e7
BizMagnets <bizmagnets@BizMagnetss-Mac-mini.local>
2025-08-19 11:52:10 +0530

    first commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by BizMagnets on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodAppTests/FoodAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppTests.swift
+//  FoodAppTests
+//
+//  Created by BizMagnets on 19/08/25.
+//
+
+import Testing
+@testable import FoodApp
+
+struct FoodAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
